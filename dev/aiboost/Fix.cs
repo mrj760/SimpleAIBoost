3dfec5383981dac7d2d43eb3e44bc19fa07cad4c
Micah Johnson <mj2carlsbad@gmail.com>
2023-08-08 22:50:22 -0700

    init

---
+++ dev/aiboost/Fix.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using TaleWorlds.MountAndBlade;
+
+namespace aiboost
+{
+    public class Fix : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            // apply harmony patches
+            var harmony = new Harmony("aiboost");
+            harmony.PatchAll();
+        }
+
+    }
+}
---
+++ dev/aiboost/Patches/Patches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace aiboost.Patches
+{
+    [HarmonyPatch]
+    public class ReduceAIMaxLevel
+    {
+        /* Set the Maximum Skill-Level for calculation to 260 instead of 350. This should make all AI stronger. */
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(AgentStatCalculateModel), "CalculateAILevel")]
+        public static bool OverrideAILevelCalculation(ref AgentStatCalculateModel __instance,
+            ref float __result, Agent agent, int relevantSkillLevel)
+        {
+            var diffmod = __instance.GetDifficultyModifier();
+            __result = MBMath.ClampFloat((float)relevantSkillLevel / 260f * diffmod, 0.01f, diffmod);
+            return false;
+        }
+    }
+}
+
+
